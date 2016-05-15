8a49b68cbf18810251a40db33126ef37cb6236e6
Ralf S. Engelschall <rse@engelschall.com>
2016-05-15 23:45:48 +0200

    place under version control

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# microkernel-mod-title
-Microkernel module for setting process title
+
+microkernel-mod-title
+=====================
+
+Microkernel module for setting process title.
+
+<p/>
+<img src="https://nodei.co/npm/microkernel-mod-title.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/microkernel-mod-title.png" alt=""/>
+
+About
+-----
+
+This is an extension module for the
+[Microkernel](http://github.com/rse/microkernel) server
+application environment, adding the capability to set the
+process title.
+
+Usage
+-----
+
+```shell
+$ npm install microkernel
+$ npm install microkernel-mod-ctx microkernel-mod-logging
+$ npm install microkernel-mod-title
+```
+
+```js
+var Microkernel = require("microkernel")
+var kernel = new Microkernel()
+
+kernel.load("microkernel-mod-title")
+```
+
+License
+-------
+
+Copyright (c) 2016 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ microkernel-mod-title.js
@@ -0,0 +1,60 @@
+/*
+**  Microkernel -- Microkernel for Server Applications
+**  Copyright (c) 2015-2016 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import os      from "os"
+import sprintf from "sprintfjs"
+
+export default class Module {
+    get module () {
+        return {
+            name:  "microkernel-mod-title",
+            tag:   "TITLE",
+            group: "BOOT",
+            after: [ "CTX", "LOGGER" ]
+        }
+    }
+    prepare (kernel) {
+        let proginfo = kernel.rs("ctx:info")
+        let procmode = kernel.rs("ctx:procmode")
+
+        /*  give initial startup hint  */
+        if (procmode === "standalone" || procmode === "master") {
+            kernel.sv("log", "title", "info",
+                sprintf("starting application %s", proginfo.app))
+            kernel.sv("log", "title", "info",
+                sprintf("running on platform %s with engine %s", proginfo.runtime, proginfo.engine))
+            kernel.sv("log", "title", "info",
+                sprintf("executing under OS %s/%s %s with %d CPUs of host %s",
+                    os.platform(), os.arch(), os.release().replace(/.*?(\d+\.\d+).*/, "$1"),
+                    os.cpus().length, os.hostname()))
+            kernel.sv("log", "title", "info",
+                sprintf("operating under %s role", procmode.toUpperCase()))
+        }
+
+        /*  provide a meaningful process title  */
+        let title = sprintf("%s", proginfo.app)
+        title = kernel.hook("title:title", "pass", title)
+        process.title = title
+    }
+}
